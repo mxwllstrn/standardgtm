3eba0c410f8bb6ae90915852141e61fe686b1e20
mxwllstrn <maxwellstern@aol.com>
2025-07-02 08:22:04 -0400

    reversion-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Outfit } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const outfitSans = Outfit({
-  variable: "--font-outfit-sans",
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -20,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${outfitSans.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
